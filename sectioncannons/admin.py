1c91840f73e0ba490d07db36db04a5e4af9bed9f
Илья Пищальников <i_pishchalnikov@rambler.ru>
2024-07-06 15:19:21 +0700

    Added and filled model Smoki;

--- sectioncannons/admin.py
+++ sectioncannons/admin.py
@@ -6,4 +6,14 @@ from . import models
 class CannonAdmin(admin.ModelAdmin):
     exclude = ["slug_name"]
     list_display = ["name", "slug_name", "image", "category"]
-    ordering = ["category"]
+    list_filter = ["category"]
+    ordering = ["name"]
+
+
+@admin.register(models.Smoki)
+class SmokiAdmin(admin.ModelAdmin):
+    list_display = ["modification", "cannon", "image",
+                    "damage_min", "damage_max", "impact_force", "recoil_force", "reload_speed", "rotation_speed",
+                    "max_dmg_range", "min_dmg_range", "weak_dmg_percent", "critical_chance", "critical_damage",
+                    "rank_required", "cost"]
+    ordering = ["modification"]
--- sectioncannons/models.py
+++ sectioncannons/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from sectionranks.models import Rank
 
 
 class Cannon(models.Model):
@@ -25,3 +26,39 @@ class Cannon(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+M_LIST = [
+    ("M0", "M0"),
+    ("M1", "M1"),
+    ("M2", "M2"),
+    ("M3", "M3")
+]
+
+
+class Smoki(models.Model):
+    cannon = models.ForeignKey(Cannon, on_delete=models.DO_NOTHING, verbose_name="Пушка")
+    modification = models.CharField(verbose_name="Модификация", max_length=2, choices=M_LIST)
+    image = models.ImageField(upload_to="Cannons-Smoki", verbose_name="Изображение")
+
+    damage_min = models.SmallIntegerField(verbose_name="Урон (мин)")
+    damage_max = models.SmallIntegerField(verbose_name="Урон (макс)")
+    impact_force = models.DecimalField(verbose_name="Сила удара", max_digits=5, decimal_places=2)
+    recoil_force = models.DecimalField(verbose_name="Отдача", max_digits=5, decimal_places=2)
+    reload_speed = models.DecimalField(verbose_name="Перезарядка", max_digits=4, decimal_places=3)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    max_dmg_range = models.DecimalField(verbose_name="Дальность макс поражения", max_digits=5, decimal_places=2)
+    min_dmg_range = models.DecimalField(verbose_name="Дальность мин поражения", max_digits=5, decimal_places=2)
+    weak_dmg_percent = models.SmallIntegerField(verbose_name="Процент слабого поражения")
+    critical_chance = models.SmallIntegerField(verbose_name="Шанс критического урона")
+    critical_damage = models.SmallIntegerField(verbose_name="Критический урон")
+
+    rank_required = models.ForeignKey(Rank, on_delete=models.DO_NOTHING, verbose_name="Требуемое звание")
+    cost = models.IntegerField(verbose_name="Стоимость")
+
+    class Meta:
+        verbose_name = "Смоки"
+        verbose_name_plural = "Смоки"
+
+    def __str__(self):
+        return f"{self.cannon.name} {self.modification}"
