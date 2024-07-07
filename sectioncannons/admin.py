cdb715b5ae8387d5670723cfdf637b9a322aaef9
Илья Пищальников <i_pishchalnikov@rambler.ru>
2024-07-07 18:14:06 +0700

    Added field "description" in Cannon model; Added abstract model "CannonCommonInfo"; Altered "Smoki" model for using abstract model inheriting; Added and filled model "Firebird";

--- sectioncannons/admin.py
+++ sectioncannons/admin.py
@@ -12,8 +12,17 @@ class CannonAdmin(admin.ModelAdmin):
 
 @admin.register(models.Smoki)
 class SmokiAdmin(admin.ModelAdmin):
-    list_display = ["modification", "cannon", "image",
+    list_display = ["modification", "image",
                     "damage_min", "damage_max", "impact_force", "recoil_force", "reload_speed", "rotation_speed",
                     "max_dmg_range", "min_dmg_range", "weak_dmg_percent", "critical_chance", "critical_damage",
+                    "rank_required", "cost"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Firebird)
+class FirebirdAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image",
+                    "damage_hp_sec", "reload_speed", "attack_duration", "rotation_speed",
+                    "max_dmg_range", "min_dmg_range", "weak_dmg_percent", "burning_time",
                     "rank_required", "cost"]
     ordering = ["modification"]
--- sectioncannons/models.py
+++ sectioncannons/models.py
@@ -15,6 +15,7 @@ class Cannon(models.Model):
     slug_name = models.SlugField(max_length=10, unique=True, blank=True)
     image = models.ImageField(upload_to="Cannons", verbose_name="Изображение")
     category = models.CharField(verbose_name="Категория", max_length=20, choices=CATEGORY_LIST)
+    description = models.TextField(verbose_name="Описание", null=True, blank=True)
 
     class Meta:
         verbose_name = "Пушка"
@@ -36,11 +37,19 @@ M_LIST = [
 ]
 
 
-class Smoki(models.Model):
+class CannonCommonInfo(models.Model):
+    """ Родительский класс, содержащий несколько общих полей, которые будут во всех дочерних """
     cannon = models.ForeignKey(Cannon, on_delete=models.DO_NOTHING, verbose_name="Пушка")
     modification = models.CharField(verbose_name="Модификация", max_length=2, choices=M_LIST)
-    image = models.ImageField(upload_to="Cannons-Smoki", verbose_name="Изображение")
+    rank_required = models.ForeignKey(Rank, on_delete=models.DO_NOTHING, verbose_name="Требуемое звание")
+    cost = models.IntegerField(verbose_name="Стоимость")
 
+    class Meta:
+        abstract = True
+
+
+class Smoki(CannonCommonInfo):
+    image = models.ImageField(upload_to="Cannons-Smoki", verbose_name="Изображение")
     damage_min = models.SmallIntegerField(verbose_name="Урон (мин)")
     damage_max = models.SmallIntegerField(verbose_name="Урон (макс)")
     impact_force = models.DecimalField(verbose_name="Сила удара", max_digits=5, decimal_places=2)
@@ -53,12 +62,28 @@ class Smoki(models.Model):
     critical_chance = models.SmallIntegerField(verbose_name="Шанс критического урона")
     critical_damage = models.SmallIntegerField(verbose_name="Критический урон")
 
-    rank_required = models.ForeignKey(Rank, on_delete=models.DO_NOTHING, verbose_name="Требуемое звание")
-    cost = models.IntegerField(verbose_name="Стоимость")
-
     class Meta:
         verbose_name = "Смоки"
         verbose_name_plural = "Смоки"
+
+    def __str__(self):
+        return f"{self.cannon.name} {self.modification}"
+
+
+class Firebird(CannonCommonInfo):
+    image = models.ImageField(upload_to="Cannons-Firebird", verbose_name="Изображение")
+    damage_hp_sec = models.SmallIntegerField(verbose_name="Урон (хп/сек)")
+    reload_speed = models.DecimalField(verbose_name="Перезарядка", max_digits=4, decimal_places=2)
+    attack_duration = models.DecimalField(verbose_name="Длительность атаки", max_digits=3, decimal_places=2)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    max_dmg_range = models.DecimalField(verbose_name="Дальность макс поражения", max_digits=4, decimal_places=2)
+    min_dmg_range = models.DecimalField(verbose_name="Дальность мин поражения", max_digits=4, decimal_places=2)
+    weak_dmg_percent = models.SmallIntegerField(verbose_name="Процент слабого поражения")
+    burning_time = models.DecimalField(verbose_name="Время горения", max_digits=3, decimal_places=1)
+
+    class Meta:
+        verbose_name = "Огнемёт"
+        verbose_name_plural = "Огнемёт"
 
     def __str__(self):
         return f"{self.cannon.name} {self.modification}"
