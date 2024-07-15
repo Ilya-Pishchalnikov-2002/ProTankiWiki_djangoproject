7761fabdde7cba14361ba44f356e40cd6c62e850
Илья Пищальников <i_pishchalnikov@rambler.ru>
2024-07-15 16:54:16 +0700

    Added and filled remaining models for sectioncannons;

--- sectioncannons/admin.py
+++ sectioncannons/admin.py
@@ -12,17 +12,93 @@ class CannonAdmin(admin.ModelAdmin):
 
 @admin.register(models.Smoki)
 class SmokiAdmin(admin.ModelAdmin):
-    list_display = ["modification", "image",
+    list_display = ["modification", "image", "rank_required", "cost",
                     "damage_min", "damage_max", "impact_force", "recoil_force", "reload_speed", "rotation_speed",
-                    "max_dmg_range", "min_dmg_range", "weak_dmg_percent", "critical_chance", "critical_damage",
-                    "rank_required", "cost"]
+                    "max_dmg_range", "min_dmg_range", "weak_dmg_percent", "critical_chance", "critical_damage"]
     ordering = ["modification"]
 
 
 @admin.register(models.Firebird)
 class FirebirdAdmin(admin.ModelAdmin):
-    list_display = ["modification", "image",
+    list_display = ["modification", "image", "rank_required", "cost",
                     "damage_hp_sec", "reload_speed", "attack_duration", "rotation_speed",
-                    "max_dmg_range", "min_dmg_range", "weak_dmg_percent", "burning_time",
-                    "rank_required", "cost"]
+                    "max_dmg_range", "min_dmg_range", "weak_dmg_percent", "burning_time"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Twins)
+class TwinsAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image", "rank_required", "cost",
+                    "damage_min", "damage_max", "impact_force", "recoil_force", "reload_speed", "rotation_speed",
+                    "projectile_speed", "max_dmg_range", "min_dmg_range", "weak_dmg_percent"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Hammer)
+class HammerAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image", "rank_required", "cost",
+                    "damage_shot", "damage_pellet", "impact_force_shot", "impact_force_pellet", "recoil_force",
+                    "reload_speed", "reload_speed_clip", "rotation_speed", "max_dmg_range", "min_dmg_range",
+                    "weak_dmg_percent", "pellet_count", "spread_angle_vertical", "spread_angle_horizonatal"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Railgun)
+class RailgunAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image", "rank_required", "cost",
+                    "damage_min", "damage_max", "impact_force", "recoil_force", "reload_speed", "shot_delay",
+                    "rotation_speed", "penetration_percent"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Isida)
+class IsidaAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image", "rank_required", "cost",
+                    "damage_hp_sec", "healing_hp_sec", "selfhealing_percent", "reload_speed", "attack_duration",
+                    "healing_idle_duration", "rotation_speed", "attack_range"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Vulcan)
+class VulcanAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image", "rank_required", "cost",
+                    "damage_hp_sec", "impact_force", "recoil_force", "overheating_time", "self_heating",
+                    "self_heat_limit", "barrel_spin_up_time", "barrel_spin_down_time", "rotation_speed",
+                    "rotation_deceleration", "max_dmg_range", "min_dmg_range", "weak_dmg_percent"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Thunder)
+class ThunderAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image", "rank_required", "cost",
+                    "damage_min", "damage_max", "impact_force", "impact_force_splash", "recoil_force",
+                    "reload_speed", "rotation_speed", "max_dmg_range", "min_dmg_range", "weak_dmg_percent",
+                    "max_dmg_radius", "min_dmg_radius", "weak_splash_dmg_percent"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Freeze)
+class FreezeAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image", "rank_required", "cost",
+                    "damage_hp_sec", "reload_speed", "attack_duration", "rotation_speed",
+                    "max_dmg_range", "min_dmg_range", "weak_dmg_percent"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Ricochet)
+class RicochetAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image", "rank_required", "cost",
+                    "damage_min", "damage_max", "impact_force", "recoil_force", "reload_speed", "energy_consumption",
+                    "charging_speed", "rotation_speed", "projectile_speed", "max_dmg_range", "min_dmg_range",
+                    "weak_dmg_percent"]
+    ordering = ["modification"]
+
+
+@admin.register(models.Shaft)
+class ShaftAdmin(admin.ModelAdmin):
+    list_display = ["modification", "image", "rank_required", "cost",
+                    "damage_sniper_min", "damage_sniper_max", "damage_arcade_min", "damage_arcade_max",
+                    "impact_force_sniper", "impact_force_arcade", "recoil_force", "reload_speed_arcade",
+                    "energy_consumption", "sniper_loading_time", "charging_speed", "rotation_speed",
+                    "aiming_speed_horiz", "aiming_speed_vertic", "penetration_percent"]
     ordering = ["modification"]
--- sectioncannons/models.py
+++ sectioncannons/models.py
@@ -83,3 +83,175 @@ class Firebird(CannonCommonInfo):
     class Meta:
         verbose_name = "Огнемёт"
         verbose_name_plural = "Огнемёт"
+
+
+class Twins(CannonCommonInfo):
+    damage_min = models.DecimalField(verbose_name="Урон (мин)", max_digits=3, decimal_places=1)
+    damage_max = models.DecimalField(verbose_name="Урон (макс)", max_digits=3, decimal_places=1)
+    impact_force = models.DecimalField(verbose_name="Сила удара", max_digits=5, decimal_places=2)
+    recoil_force = models.DecimalField(verbose_name="Отдача", max_digits=5, decimal_places=2)
+    reload_speed = models.DecimalField(verbose_name="Перезарядка", max_digits=3, decimal_places=2)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    projectile_speed = models.DecimalField(verbose_name="Скорость снаряда", max_digits=4, decimal_places=2)
+    max_dmg_range = models.DecimalField(verbose_name="Дальность макс поражения", max_digits=4, decimal_places=2)
+    min_dmg_range = models.DecimalField(verbose_name="Дальность мин поражения", max_digits=4, decimal_places=2)
+    weak_dmg_percent = models.DecimalField(verbose_name="Процент слабого поражения", max_digits=4, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Твинс"
+        verbose_name_plural = "Твинс"
+
+
+class Hammer(CannonCommonInfo):
+    damage_shot = models.DecimalField(verbose_name="Урон выстрела", max_digits=4, decimal_places=1)
+    damage_pellet = models.DecimalField(verbose_name="Урон дробинки", max_digits=3, decimal_places=1)
+    impact_force_shot = models.DecimalField(verbose_name="Сила удара выстрела", max_digits=5, decimal_places=2)
+    impact_force_pellet = models.DecimalField(verbose_name="Сила удара дробинки", max_digits=4, decimal_places=2)
+    recoil_force = models.DecimalField(verbose_name="Отдача", max_digits=5, decimal_places=2)
+    reload_speed = models.DecimalField(verbose_name="Перезарядка", max_digits=4, decimal_places=3)
+    reload_speed_clip = models.DecimalField(verbose_name="Зарядка обоймы", max_digits=4, decimal_places=3)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    max_dmg_range = models.DecimalField(verbose_name="Дальность макс поражения", max_digits=4, decimal_places=2)
+    min_dmg_range = models.DecimalField(verbose_name="Дальность мин поражения", max_digits=4, decimal_places=2)
+    weak_dmg_percent = models.SmallIntegerField(verbose_name="Процент слабого поражения")
+    pellet_count = models.SmallIntegerField(verbose_name="Количество дробинок")
+    spread_angle_vertical = models.SmallIntegerField(verbose_name="Угол разброса (вертикальный)")
+    spread_angle_horizonatal = models.SmallIntegerField(verbose_name="Угол разброса (горизонтальный)")
+
+    class Meta:
+        verbose_name = "Молот"
+        verbose_name_plural = "Молот"
+
+
+class Railgun(CannonCommonInfo):
+    damage_min = models.SmallIntegerField(verbose_name="Урон (мин)")
+    damage_max = models.SmallIntegerField(verbose_name="Урон (макс)")
+    impact_force = models.DecimalField(verbose_name="Сила удара", max_digits=5, decimal_places=2)
+    recoil_force = models.DecimalField(verbose_name="Отдача", max_digits=5, decimal_places=2)
+    reload_speed = models.DecimalField(verbose_name="Перезарядка", max_digits=4, decimal_places=3)
+    shot_delay = models.DecimalField(verbose_name="Задержка перед выстрелом", max_digits=4, decimal_places=3)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    penetration_percent = models.DecimalField(verbose_name="Процент прострела", max_digits=4, decimal_places=1)
+
+    class Meta:
+        verbose_name = "Рельса"
+        verbose_name_plural = "Рельса"
+
+
+class Isida(CannonCommonInfo):
+    damage_hp_sec = models.SmallIntegerField(verbose_name="Урон (хп/сек)")
+    healing_hp_sec = models.SmallIntegerField(verbose_name="Лечение (хп/сек)")
+    selfhealing_percent = models.DecimalField(verbose_name="Процент самолечения", max_digits=3, decimal_places=1)
+    reload_speed = models.DecimalField(verbose_name="Перезарядка", max_digits=4, decimal_places=2)
+    attack_duration = models.DecimalField(verbose_name="Длительность атаки", max_digits=3, decimal_places=2)
+    healing_idle_duration = models.DecimalField(verbose_name="Длительность лечения/холостой атаки",
+                                                max_digits=4, decimal_places=2)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    attack_range = models.DecimalField(verbose_name="Дальность атаки", max_digits=4, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Изида"
+        verbose_name_plural = "Изида"
+
+
+class Vulcan(CannonCommonInfo):
+    damage_hp_sec = models.SmallIntegerField(verbose_name="Урон (хп/сек)")
+    impact_force = models.DecimalField(verbose_name="Сила удара", max_digits=5, decimal_places=2)
+    recoil_force = models.DecimalField(verbose_name="Отдача", max_digits=5, decimal_places=2)
+    overheating_time = models.DecimalField(verbose_name="Время до перегрева", max_digits=4, decimal_places=3)
+    self_heating = models.DecimalField(verbose_name="Самонагрев", max_digits=3, decimal_places=2)
+    self_heat_limit = models.DecimalField(verbose_name="Ограничение на температуру", max_digits=3, decimal_places=2)
+    barrel_spin_up_time = models.DecimalField(verbose_name="Раскрутка стволов", max_digits=3, decimal_places=2)
+    barrel_spin_down_time = models.DecimalField(verbose_name="Остановка стволов", max_digits=3, decimal_places=2)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    rotation_deceleration = models.DecimalField(verbose_name="Замедление поворта при стрельбе",
+                                                max_digits=4, decimal_places=1)
+    max_dmg_range = models.DecimalField(verbose_name="Дальность макс поражения", max_digits=4, decimal_places=2)
+    min_dmg_range = models.DecimalField(verbose_name="Дальность мин поражения", max_digits=5, decimal_places=2)
+    weak_dmg_percent = models.SmallIntegerField(verbose_name="Процент слабого поражения")
+
+    class Meta:
+        verbose_name = "Вулкан"
+        verbose_name_plural = "Вулкан"
+
+
+class Thunder(CannonCommonInfo):
+    damage_min = models.SmallIntegerField(verbose_name="Урон (мин)")
+    damage_max = models.SmallIntegerField(verbose_name="Урон (макс)")
+    impact_force = models.DecimalField(verbose_name="Сила удара", max_digits=5, decimal_places=2)
+    impact_force_splash = models.DecimalField(verbose_name="Сила удара сплеша", max_digits=5, decimal_places=2)
+    recoil_force = models.DecimalField(verbose_name="Отдача", max_digits=5, decimal_places=2)
+    reload_speed = models.DecimalField(verbose_name="Перезарядка", max_digits=4, decimal_places=3)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    max_dmg_range = models.DecimalField(verbose_name="Дальность макс поражения", max_digits=4, decimal_places=2)
+    min_dmg_range = models.DecimalField(verbose_name="Дальность мин поражения", max_digits=5, decimal_places=2)
+    weak_dmg_percent = models.SmallIntegerField(verbose_name="Процент слабого поражения")
+    max_dmg_radius = models.DecimalField(verbose_name="Радиус макс поражения", max_digits=4, decimal_places=2)
+    min_dmg_radius = models.DecimalField(verbose_name="Радиус мин поражения", max_digits=4, decimal_places=2)
+    weak_splash_dmg_percent = models.SmallIntegerField(verbose_name="Процент слабого поражения сплешом")
+
+    class Meta:
+        verbose_name = "Гром"
+        verbose_name_plural = "Гром"
+
+
+class Freeze(CannonCommonInfo):
+    damage_hp_sec = models.SmallIntegerField(verbose_name="Урон (хп/сек)")
+    reload_speed = models.DecimalField(verbose_name="Перезарядка", max_digits=4, decimal_places=2)
+    attack_duration = models.DecimalField(verbose_name="Длительность атаки", max_digits=3, decimal_places=2)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    max_dmg_range = models.DecimalField(verbose_name="Дальность макс поражения", max_digits=4, decimal_places=2)
+    min_dmg_range = models.DecimalField(verbose_name="Дальность мин поражения", max_digits=4, decimal_places=2)
+    weak_dmg_percent = models.SmallIntegerField(verbose_name="Процент слабого поражения")
+
+    class Meta:
+        verbose_name = "Фриз"
+        verbose_name_plural = "Фриз"
+
+
+class Ricochet(CannonCommonInfo):
+    damage_min = models.SmallIntegerField(verbose_name="Урон (мин)")
+    damage_max = models.SmallIntegerField(verbose_name="Урон (макс)")
+    impact_force = models.DecimalField(verbose_name="Сила удара", max_digits=5, decimal_places=2)
+    recoil_force = models.DecimalField(verbose_name="Отдача", max_digits=5, decimal_places=2)
+    reload_speed = models.DecimalField(verbose_name="Перезарядка выстрелов", max_digits=4, decimal_places=3)
+    energy_consumption = models.DecimalField(verbose_name="Процент энергии на выстрел", max_digits=4, decimal_places=2)
+    charging_speed = models.DecimalField(verbose_name="Перезарядка всего баллона", max_digits=4, decimal_places=2)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    projectile_speed = models.DecimalField(verbose_name="Скорость снаряда", max_digits=5, decimal_places=2)
+    max_dmg_range = models.DecimalField(verbose_name="Дальность макс поражения", max_digits=4, decimal_places=2)
+    min_dmg_range = models.DecimalField(verbose_name="Дальность мин поражения", max_digits=5, decimal_places=2)
+    weak_dmg_percent = models.SmallIntegerField(verbose_name="Процент слабого поражения")
+
+    class Meta:
+        verbose_name = "Рикошет"
+        verbose_name_plural = "Рикошет"
+
+
+class Shaft(CannonCommonInfo):
+    damage_sniper_min = models.SmallIntegerField(verbose_name="Урон прицельный(мин)")
+    damage_sniper_max = models.SmallIntegerField(verbose_name="Урон прицельный(макс)")
+    damage_arcade_min = models.SmallIntegerField(verbose_name="Урон навскидку(мин)")
+    damage_arcade_max = models.SmallIntegerField(verbose_name="Урон навскидку(макс)")
+    impact_force_sniper = models.DecimalField(verbose_name="Сила удара прицельного выстрела", max_digits=5,
+                                              decimal_places=2)
+    impact_force_arcade = models.DecimalField(verbose_name="Сила удара выстрела навскидку", max_digits=5,
+                                              decimal_places=2)
+    recoil_force = models.DecimalField(verbose_name="Отдача", max_digits=5, decimal_places=2)
+    reload_speed_arcade = models.DecimalField(verbose_name="Перезарядка выстрелов навскидку", max_digits=4,
+                                              decimal_places=3)
+    energy_consumption = models.DecimalField(verbose_name="Процент энергии на выстрел навскидку", max_digits=4,
+                                             decimal_places=2)
+    sniper_loading_time = models.DecimalField(verbose_name="Время зарядки прицельного выстрела", max_digits=3,
+                                              decimal_places=2)
+    charging_speed = models.DecimalField(verbose_name="Перезарядка всего баллона", max_digits=4, decimal_places=2)
+    rotation_speed = models.DecimalField(verbose_name="Скорость поворота", max_digits=4, decimal_places=1)
+    aiming_speed_horiz = models.DecimalField(verbose_name="Скорость прицеливания по горизонтали", max_digits=4,
+                                             decimal_places=2)
+    aiming_speed_vertic = models.DecimalField(verbose_name="Скорость прицеливания по вертикали", max_digits=4,
+                                              decimal_places=2)
+    penetration_percent = models.DecimalField(verbose_name="Процент прострела", max_digits=4, decimal_places=1)
+
+    class Meta:
+        verbose_name = "Шафт"
+        verbose_name_plural = "Шафт"
