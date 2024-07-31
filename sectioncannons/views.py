8de4a4db18324845c3ba42f1806a9501eab88a49
Илья Пищальников <i_pishchalnikov@rambler.ru>
2024-07-31 16:59:20 +0700

    Improved CSS for homepage; Developed cannons page (template, CSS, view);

--- sectioncannons/views.py
+++ sectioncannons/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
+from sectioncannons.models import Cannon
 
 
 class CannonsPageView(View):
     def get(self, request):
-        return render(request, "sectionguns/cannons_main.html")
+        cannons_close = Cannon.objects.filter(category="Ближний бой")
+        cannons_short = Cannon.objects.filter(category="Малая дальность")
+        cannons_meduim = Cannon.objects.filter(category="Средняя дальность")
+        cannons_long = Cannon.objects.filter(category="Большая дальность")
+
+        data = {"close_combat": cannons_close,
+                "short_range": cannons_short,
+                "medium_range": cannons_meduim,
+                "long_range": cannons_long}
+
+        return render(request, "sectionguns/cannons_main.html", context=data)
